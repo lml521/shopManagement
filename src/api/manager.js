4c41abc4bf1fbc1a9495fa10e8456e16ab90ee4c
lml <2084387897@qq.com>
2023-02-21 15:00:37 +0800

    cs

--- src/api/manager.js
+++ src/api/manager.js
@@ -7,16 +7,14 @@ export const getTableList = (page) => {
         method: "GET",
     })
 }
-
-// 搜索 
+// 搜索
 export const getSearch=(page,title)=>{
     return request({
         url:`admin/manager/${page}?keyword=${title}`,
         method: "GET",
     })
 }
-
-// 切换 状态 值 
+// 切换 状态 值
 export const getChangeStatus=(id,status)=>{
     return request({
         url:`/admin/manager/${id}/update_status`,
